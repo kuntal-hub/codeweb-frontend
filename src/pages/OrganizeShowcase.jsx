af2d89b6524f3b7484cd8b8b208791f89a30763b
kuntal <km7999319@gmail.com>
2024-05-20 22:09:40 +0530

    Update OrganizeShowcase page with responsive font size and centered text

--- src/pages/OrganizeShowcase.jsx
+++ src/pages/OrganizeShowcase.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default function OrganizeShowcase() {
   return (
     <div className='h-screen w-screen fixed top-0 left-0 bg-gray-950 m-0 p-0'>
-        <div className='grid place-content-center w-full h-full text-white font-bold text-2xl sm:text-4xl'>
+        <div className='text-center my-[30vh] w-full h-full text-white font-bold text-2xl sm:text-4xl'>
             Page under construction
             <p className=' text-sm w-screen text-center mt-5'>
               Go back to <Link className='text-blue-700 hover:underline' to='/'>Home</Link>
