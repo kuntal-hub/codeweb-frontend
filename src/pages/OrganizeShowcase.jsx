46a8940fccb1e646d8d7464d0a5a6084cb1b8f75
kuntal <km7999319@gmail.com>
2024-05-20 22:03:16 +0530

    chore: Update OrganizeShowcase page with construction message and link to home

--- src/pages/OrganizeShowcase.jsx
+++ src/pages/OrganizeShowcase.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function OrganizeShowcase() {
   return (
     <div className='h-screen w-screen fixed top-0 left-0 bg-gray-950 m-0 p-0'>
         <div className='grid place-content-center w-full h-full text-white font-bold text-4xl'>
-            Laura Lasun 
+            Page under construction
+            <p className=' text-sm'>
+              Go back to <Link className='text-blue-700 hover:underline' to='/'>Home</Link>
+            </p>
         </div>
     </div>
   )
